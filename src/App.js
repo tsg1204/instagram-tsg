6bd63332d5e5f668f63f970f2c2f430f4cb33fcc
Tatiana Grigorieva <tsg1204@gmail.com>
2020-12-21 16:00:50 -0500

    layout and not found page added

--- src/App.js
+++ src/App.js
@@ -13,10 +13,10 @@ function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/" component={FeedPage} />
+        <Route exact path="/" component={FeedPage} />
         <Route path="/explore" component={ExplorePage} />
-        <Route path="/:username" component={ProfilePage} />
-        <Route path="/p/:postId" component={PostPage} />
+        <Route exact path="/:username" component={ProfilePage} />
+        <Route exact path="/p/:postId" component={PostPage} />
         <Route path="/accounts/edit" component={EditProfilePage} />
         <Route path="/accounts/login" component={LoginPage} />
         <Route path="/accounts/emailsignup" component={SignUpPage} />
