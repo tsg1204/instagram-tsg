29114f6383ed224f2f42632a67d65d97a514b53d
Tatiana Grigorieva <tsg1204@gmail.com>
2020-12-24 15:38:16 -0500

    add feed side and follow buttons

--- src/components/feed/FeedSideSuggestions.js
+++ src/components/feed/FeedSideSuggestions.js
@@ -1,10 +1,28 @@
 import React from "react";
 import { useFeedSideSuggestionsStyles } from "../../styles";
+import { Paper, Typography } from "@material-ui/core";
+import UserCard from "../shared/UserCard";
+import { getDefaultUser } from '../../data';
+import FollowButton from '../shared/FollowButton';
 
 function FeedSideSuggestions() {
-  useFeedSideSuggestionsStyles();
+  const classes = useFeedSideSuggestionsStyles();
 
-  return <div>FeedSideSuggestions</div>;
+  return (
+    <article>
+      <Paper>
+        <Typography color="textSecondary" variant="subtitle2" component="h2" align="left" gutterBottom className={classes.typography}>
+          Suggestions For You
+        </Typography>
+        {Array.from({ length: 5}, () => getDefaultUser()).map(user => (
+          <div key={user.id} className={classes.card}>
+            <UserCard user={user} />
+            <FollowButton side />
+          </div>
+        ))}
+      </Paper>
+    </article>
+  )
 }
 
 export default FeedSideSuggestions;
