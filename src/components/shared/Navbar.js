a00e61b01ed6021622ae39c52de1bdc857775bd1
Tatiana Grigorieva <tsg1204@gmail.com>
2020-12-24 16:46:26 -0500

    refactor navbar addedhome explore like profile icons

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -1,16 +1,26 @@
 import React from "react";
 import { useNavbarStyles } from "../../styles";
-import { AppBar } from "@material-ui/core";
-import { Link } from "react-router-dom";
-import logo from '../../images/logo.png'
+import { AppBar, Avatar, Hidden, InputBase } from "@material-ui/core";
+import { Link, useHistory } from "react-router-dom";
+import logo from '../../images/logo.png';
+import { LoadingIcon, AddIcon, LikeIcon, LikeActiveIcon, ExploreIcon, ExploreActiveIcon, HomeIcon, HomeActiveIcon  } from '../../icons';
+import { defaultCurrentUser } from '../../data';
 
-function Navbar() {
+function Navbar( {minimalNavbar }) {
   const classes = useNavbarStyles();
+  const history = useHistory();
+  const path = history.location.pathname;
 
   return (
     <AppBar className={classes.appBar}>
       <section className={classes.section}>
         <Logo />
+        { !minimalNavbar && (
+          <>
+            <Search /> 
+            <Links path={path} /> 
+          </>
+        )}
       </section>
     </AppBar>
   )
@@ -26,6 +36,70 @@ function Logo() {
           <img src={logo} alt="Instagram" className={classes.logo} />
         </div>
       </Link>
+    </div>
+  )
+}
+
+function Search() {
+  const classes = useNavbarStyles();
+  const [query, setQuery] = React.useState('');
+  let loading = false;
+
+  function handleClearInput() {
+    setQuery('');
+  }
+
+  return (
+    <Hidden xsDown>
+      <InputBase 
+        className={classes.input}
+        onChange={event => setQuery(event.target.value)}
+        startAdornment={<span className={classes.searchIcon} />}
+        endAdornment={
+          loading ? (
+            <LoadingIcon />
+          ) : (
+            <span onClick={handleClearInput} className={classes.clearIcon}></span>
+          )
+        }
+        placeholder="Search"
+        value={query}
+      />
+    </Hidden>
+  )
+}
+
+function Links({ path }) {
+  const classes = useNavbarStyles();
+  const [showList, setList] = React.useState(false);
+
+function handleToggleList() {
+  setList(prev => !prev);
+}
+
+  return (
+    <div className={classes.linksContainer}>
+      <div className={classes.linksWrapper}>
+        <Hidden xsDown>
+          <AddIcon />
+        </Hidden>
+        <Link to="/">
+          {path === "/" ? <HomeActiveIcon /> : <HomeIcon />}
+        </Link>
+        <Link to="/explore">
+          {path === "/" ? <ExploreActiveIcon /> : <ExploreIcon />}
+        </Link>
+        <div className={classes.notifications} onClick={handleToggleList}>
+          {showList ? <LikeActiveIcon /> : <LikeIcon />}
+        </div>
+        <Link to={`/${defaultCurrentUser.username}`}>
+          <div className={path === `/${defaultCurrentUser.username}` ? classes.profileActive : ""}></div>
+          <Avatar 
+            src={defaultCurrentUser.profile_image}
+            className={classes.profileImage}
+          />
+        </Link>
+      </div>
     </div>
   )
 }
--- src/pages/not-found.js
+++ src/pages/not-found.js
@@ -4,7 +4,7 @@ import { Typography } from "@material-ui/core";
 import { Link } from "react-router-dom";
 
 function NotFoundPage() {
-  return <Layout title="Page Not Found" marginTop={120}>
+  return <Layout minimalNavbar title="Page Not Found" marginTop={120}>
     <Typography variant="h5" align="center" paragraph>
       Sorry, this page isn't available.
     </Typography>
