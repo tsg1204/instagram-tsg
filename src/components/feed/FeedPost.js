fe7a91bd94b2acd9f9871a5cf4262fce4fad0765
Tatiana Grigorieva <tsg1204@gmail.com>
2020-12-24 15:07:05 -0500

    refactor feed post and added user card page

--- src/components/feed/FeedPost.js
+++ src/components/feed/FeedPost.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { useFeedPostStyles } from "../../styles";
 import UserCard from "../shared/UserCard";
-import { CommentIcon, MoreIcon, ShareIcon } from '../../icons';
+import { CommentIcon, MoreIcon, ShareIcon, UnlikeIcon, LikeIcon, RemoveIcon, SaveIcon } from '../../icons';
 import { Link } from "react-router-dom";
-import { Button, Hidden, Typography, Divider } from "@material-ui/core";
+import { Button, Hidden, Typography, Divider, TextField } from "@material-ui/core";
 import HTMLEllipsis from "react-lines-ellipsis/lib/html";
 
 function FeedPost({ post }) {
@@ -24,7 +24,7 @@ function FeedPost({ post }) {
           <img src={media} alt="Post media" className={classes.image} />
         </div>
         {/* Feed Post Buttons */}
-        <div className={classes.postButtonWrapper}>
+        <div className={classes.postButtonsWrapper}>
           <div className={classes.postButtons}>
             <LikeButton />
             <Link to={`/p/${id}`}>
@@ -45,7 +45,7 @@ function FeedPost({ post }) {
             {showCaption ? (
               <Typography variant="body2" component="span" dangerouslySetInnerHTML={{ __html: caption}} />
             ) : (
-              <div>
+              <div className={classes.captionWrapper}>
                 <HTMLEllipsis 
                   unsafeHTML={caption}
                   className={classes.caption}
@@ -92,15 +92,70 @@ function FeedPost({ post }) {
 }
 
 function LikeButton() {
-  return <>LikeButton</>
+  const classes = useFeedPostStyles();
+  const [liked, setLiked] = React.useState(false);
+  const Icon = liked ? UnlikeIcon : LikeIcon;
+  const className = liked ? classes.liked : classes.like;
+  const onClick = liked ? handleUnlike : handleLike;
+
+  function handleLike() {
+    console.log('like');
+    setLiked(true);
+  }
+
+  function handleUnlike() {
+    console.log('unlike');
+    setLiked(false);
+  }
+
+  return <Icon className={className} onClick={onClick} />
 }
 
 function SaveButton() {
-  return <>SaveButton</>
+  const classes = useFeedPostStyles();
+  const [saved, setSaved] = React.useState(false);
+  const Icon = saved ? RemoveIcon : SaveIcon;
+  const onClick = saved ? handleRemove : handleSave;
+
+  function handleSave() {
+    console.log('save');
+    setSaved(true);
+  }
+
+  function handleRemove() {
+    console.log('remove');
+    setSaved(false);
+  }
+
+  return <Icon className={classes.saveIcon} onClick={onClick} />
 }
 
 function Comment() {
-  return <>Comment</>
+  const classes = useFeedPostStyles();
+  const [content, setContent] = React.useState('');
+
+  return (
+    <div className={classes.commentContainer}>
+      <TextField
+        fullWidth
+        value={content}
+        placeholder="Add a comment..."
+        multiline
+        rowsMax={2}
+        row={1}
+        onChange={event => setContent(event.target.value)}
+        InputProps={{
+          classes: {
+            root: classes.root,
+            underline: classes.underline
+          }
+        }}
+      />
+      <Button color="primary" className={classes.commentButton} disabled={!content.trim()}>
+        Post
+      </Button>
+    </div>
+  )
 }
 
 export default FeedPost;
