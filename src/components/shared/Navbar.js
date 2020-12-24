540ecb9889128046b317e5e71908194151afb889
Tatiana Grigorieva <tsg1204@gmail.com>
2020-12-24 17:30:48 -0500

    added tooltip to navbar

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -1,10 +1,17 @@
 import React from "react";
-import { useNavbarStyles } from "../../styles";
-import { AppBar, Avatar, Hidden, InputBase } from "@material-ui/core";
+import { useNavbarStyles, WhiteTooltip } from "../../styles";
+import {   AppBar,
+  Hidden,
+  InputBase,
+  Avatar,
+  Fade,
+  Grid,
+  Typography,
+  Zoom } from "@material-ui/core";
 import { Link, useHistory } from "react-router-dom";
 import logo from '../../images/logo.png';
 import { LoadingIcon, AddIcon, LikeIcon, LikeActiveIcon, ExploreIcon, ExploreActiveIcon, HomeIcon, HomeActiveIcon  } from '../../icons';
-import { defaultCurrentUser } from '../../data';
+import { defaultCurrentUser, getDefaultUser } from '../../data';
 
 function Navbar( {minimalNavbar }) {
   const classes = useNavbarStyles();
@@ -17,7 +24,7 @@ function Navbar( {minimalNavbar }) {
         <Logo />
         { !minimalNavbar && (
           <>
-            <Search /> 
+            <Search history={history} /> 
             <Links path={path} /> 
           </>
         )}
@@ -40,10 +47,18 @@ function Logo() {
   )
 }
 
-function Search() {
+function Search({ history }) {
   const classes = useNavbarStyles();
+  const [loading, setLoading] = React.useState(false);
+  const [results, setResults] = React.useState([]);
   const [query, setQuery] = React.useState('');
-  let loading = false;
+
+  const hasResults = Boolean(query) && results.length > 0;
+
+  React.useEffect(() => {
+    if ( !query.trim()) return;
+    setResults(Array.from({length: 5}, () => getDefaultUser()));
+  }, [query])
 
   function handleClearInput() {
     setQuery('');
@@ -51,6 +66,40 @@ function Search() {
 
   return (
     <Hidden xsDown>
+      <WhiteTooltip 
+        arrow
+        interactive
+        TransitionComponent={Fade}
+        open={hasResults}
+        title={
+          hasResults && (
+            <Grid className={classes.resultContainer} container>
+            {results.map(result => (
+              <Grid key={result.id} item className={classes.resultLink}
+                onClick={() => {
+                  history.push(`/${result.username}`);
+                  handleClearInput();
+                }}
+              >
+                <div className={classes.resultWrapper}>
+                  <div className={classes.avatarWrapper}>
+                    <Avatar src={result.profile_image} alt="user avatar" />
+                  </div>
+                  <div className={classes.nameWrapper}>
+                    <Typography variant="body1">
+                      {result.username}
+                    </Typography>
+                    <Typography variant="body2">
+                      {result.name}
+                    </Typography>
+                  </div>
+                </div>
+              </Grid>
+            ))}
+            </Grid>
+          )
+        }
+      >
       <InputBase 
         className={classes.input}
         onChange={event => setQuery(event.target.value)}
@@ -65,6 +114,7 @@ function Search() {
         placeholder="Search"
         value={query}
       />
+      </WhiteTooltip>
     </Hidden>
   )
 }
