d4d8f8b255b7a444f3afd441becf1715707b25c1
Tatiana Grigorieva <tsg1204@gmail.com>
2021-01-06 14:53:59 -0500

    deployed to vercel

---
+++ build/precache-manifest.3fc173f3ab94ceb0522f793093000293.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "411ed32fae2977b6d705f0a849f71cd7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b0ea91a34c808b7b280",
+    "url": "/static/css/2.1c9ef103.chunk.css"
+  },
+  {
+    "revision": "6b0ea91a34c808b7b280",
+    "url": "/static/js/2.ddcba73a.chunk.js"
+  },
+  {
+    "revision": "30cf246dc78558165e475dda08f2a921",
+    "url": "/static/js/2.ddcba73a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6d99d714844d179409a",
+    "url": "/static/js/3.c0d193ef.chunk.js"
+  },
+  {
+    "revision": "edc64f1683d100dbdf5c",
+    "url": "/static/js/main.f88d66d2.chunk.js"
+  },
+  {
+    "revision": "b01d3148edb2f220c22e",
+    "url": "/static/js/runtime-main.a59b4b3c.js"
+  },
+  {
+    "revision": "cbb9b56a5cf892318fc8ab502f9d3ba7",
+    "url": "/static/media/facebook-icon-blue.cbb9b56a.svg"
+  },
+  {
+    "revision": "a9a13771e5dcd73931e0b61df2faac62",
+    "url": "/static/media/icon-sheet-2.a9a13771.png"
+  },
+  {
+    "revision": "3dfb71b4bb35ca8b35728b9475b7d861",
+    "url": "/static/media/icon-sheet.3dfb71b4.png"
+  },
+  {
+    "revision": "8652aac5bac4c85206fcebb3c40b024c",
+    "url": "/static/media/post_media.8652aac5.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
