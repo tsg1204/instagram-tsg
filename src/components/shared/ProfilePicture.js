a957ff31891fb735a3aef0522039f9bc3bce1fb7
Tatiana Grigorieva <tsg1204@gmail.com>
2021-01-06 13:24:57 -0500

    profile page and comp added

--- src/components/shared/ProfilePicture.js
+++ src/components/shared/ProfilePicture.js
@@ -1,10 +1,24 @@
-import React from "react";
-import { useProfilePictureStyles } from "../../styles";
+import React from 'react';
+import { useProfilePictureStyles } from '../../styles';
+import { Person } from '@material-ui/icons';
+import post_media from '../../images/post_media.png';
 
-function ProfilePicture() {
-  useProfilePictureStyles();
+function ProfilePicture({ size, image = post_media, isOwner }) {
+  const classes = useProfilePictureStyles({ size, isOwner });
 
-  return <div>ProfilePicture</div>;
+  return (
+    <section className={classes.section}>
+      {image ? (
+        <div className={classes.wrapper}>
+          <img src={image} alt="user profile" className={classes.image} />
+        </div>
+      ) : (
+        <div className={classes.wrapper}>
+          <Person className={classes.person} />
+        </div>
+      )}
+    </section>
+  );
 }
 
 export default ProfilePicture;
