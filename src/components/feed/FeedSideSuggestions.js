3d35c7fa6f827d76d6694dbcec4a798a5414cac5
Tatiana Grigorieva <tsg1204@gmail.com>
2021-01-17 20:28:50 -0500

    refactor feed page

--- src/components/feed/FeedSideSuggestions.js
+++ src/components/feed/FeedSideSuggestions.js
@@ -1,33 +1,47 @@
-import React from "react";
-import { useFeedSideSuggestionsStyles } from "../../styles";
-import { Paper, Typography } from "@material-ui/core";
-import UserCard from "../shared/UserCard";
-import { getDefaultUser } from '../../data';
+import React from 'react';
+import { useFeedSideSuggestionsStyles } from '../../styles';
+import { Paper, Typography } from '@material-ui/core';
+import UserCard from '../shared/UserCard';
+//import { getDefaultUser } from '../../data';
 import FollowButton from '../shared/FollowButton';
 import { LoadingIcon } from '../../icons';
+import { useQuery } from '@apollo/react-hooks';
+import { SUGGEST_USERS } from '../../graphql/queries';
+import { UserContext } from '../../App';
 
 function FeedSideSuggestions() {
   const classes = useFeedSideSuggestionsStyles();
-  let loading = false;
+  const { me, followerIds } = React.useContext(UserContext);
+  const variables = { limit: 5, followerIds, createdAt: me.created_at };
+  const { data, loading } = useQuery(SUGGEST_USERS, { variables });
+  //let loading = false;
 
   return (
-    <article>
-      <Paper>
-        <Typography color="textSecondary" variant="subtitle2" component="h2" align="left" gutterBottom className={classes.typography}>
+    <article className={classes.article}>
+      <Paper className={classes.paper}>
+        <Typography
+          color="textSecondary"
+          variant="subtitle2"
+          component="h2"
+          align="left"
+          gutterBottom
+          className={classes.typography}
+        >
           Suggestions For You
         </Typography>
         {loading ? (
           <LoadingIcon />
-        ) :
-          Array.from({ length: 5}, () => getDefaultUser()).map(user => (
-          <div key={user.id} className={classes.card}>
-            <UserCard user={user} />
-            <FollowButton side />
-          </div>
-        ))}
+        ) : (
+          data.users.map((user) => (
+            <div key={user.id} className={classes.card}>
+              <UserCard user={user} />
+              <FollowButton id={user.id} side />
+            </div>
+          ))
+        )}
       </Paper>
     </article>
-  )
+  );
 }
 
 export default FeedSideSuggestions;
--- src/pages/feed.js
+++ src/pages/feed.js
@@ -2,19 +2,28 @@ import React from 'react';
 import { useFeedPageStyles } from '../styles';
 import Layout from '../components/shared/Layout';
 import UserCard from '../components/shared/UserCard';
-import { getDefaultPost } from '../data';
+//import { getDefaultPost } from '../data';
 import FeedSideSuggestions from '../components/feed/FeedSideSuggestions';
 import { Hidden } from '@material-ui/core';
 import LoadingScreen from '../components/shared/LoadingScreen';
 import { LoadingLargeIcon } from '../icons';
 import FeedPostSkeleton from '../components/feed/FeedPostSkeleton';
+import { UserContext } from '../App';
+import { useQuery } from '@apollo/react-hooks';
+import { GET_FEED } from '../graphql/queries';
+//import usePageBottom from '../utils/usePageBottom';
+
 const FeedPost = React.lazy(() => import('../components/feed/FeedPost'));
 
 function FeedPage() {
   const classes = useFeedPageStyles();
+  const { me, feedIds } = React.useContext(UserContext);
   const [isEndOfFeed] = React.useState(false);
+  const variables = { feedIds, limit: 2 };
+  const { data, loading, fetchMore } = useQuery(GET_FEED, { variables });
+  //const isPageBottom = usePageBottom();
 
-  let loading = false;
+  //let loading = false;
   if (loading) return <LoadingScreen />;
 
   return (
@@ -22,19 +31,17 @@ function FeedPage() {
       <div className={classes.container}>
         {/*Feed posts */}
         <div>
-          {Array.from({ length: 5 }, () => getDefaultPost()).map(
-            (post, index) => (
-              <React.Suspense key={post.id} fallback={<FeedPostSkeleton />}>
-                <FeedPost post={post} index={index} />
-              </React.Suspense>
-            )
-          )}
+          {data.posts.map((post, index) => (
+            <React.Suspense key={post.id} fallback={<FeedPostSkeleton />}>
+              <FeedPost post={post} index={index} />
+            </React.Suspense>
+          ))}
         </div>
         {/* Sidebar */}
         <Hidden smDown>
           <div className={classes.sidebarContainer}>
             <div className={classes.sidebarWrapper}>
-              <UserCard avatarSize={50} />
+              <UserCard user={me} avatarSize={50} />
               <FeedSideSuggestions />
             </div>
           </div>
