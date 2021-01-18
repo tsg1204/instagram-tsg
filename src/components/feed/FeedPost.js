32502b0277ab04aa59fb920c7420ab353ddafa54
Tatiana Grigorieva <tsg1204@gmail.com>
2021-01-18 12:07:20 -0500

    like button func added to feed page

--- src/components/feed/FeedPost.js
+++ src/components/feed/FeedPost.js
@@ -23,6 +23,16 @@ import FollowSuggestions from '../shared/FollowSuggestions';
 import OptionsDialog from '../shared/OptionsDialog';
 import { formatDateToNow } from '../../utils/formatDate';
 import Img from 'react-graceful-image';
+import {
+  SAVE_POST,
+  UNSAVE_POST,
+  LIKE_POST,
+  UNLIKE_POST,
+  CREATE_COMMENT,
+} from '../../graphql/mutations';
+import { GET_FEED } from '../../graphql/queries';
+import { useMutation } from '@apollo/react-hooks';
+import { UserContext } from '../../App';
 
 function FeedPost({ post, index }) {
   const classes = useFeedPostStyles();
@@ -66,12 +76,12 @@ function FeedPost({ post, index }) {
         {/* Feed Post Buttons */}
         <div className={classes.postButtonsWrapper}>
           <div className={classes.postButtons}>
-            <LikeButton />
+            <LikeButton likes={likes} postId={id} authorId={user.id} />
             <Link to={`/p/${id}`}>
               <CommentIcon />
             </Link>
             <ShareIcon />
-            <SaveButton />
+            <SaveButton savedPosts={saved_posts} postId={id} />
           </div>
           <Typography className={classes.likes} variant="subtitle2">
             <span>{likesCount === 1 ? '1 like' : `${likesCount} likes`}</span>
@@ -156,27 +166,60 @@ function FeedPost({ post, index }) {
   );
 }
 
-function LikeButton() {
+function LikeButton({ likes, postId, authorId }) {
   const classes = useFeedPostStyles();
-  const [liked, setLiked] = React.useState(false);
+  const { currentUserId, feedIds } = React.useContext(UserContext);
+  const isAlreadyLiked = likes.some(({ user_id }) => user_id === currentUserId);
+  const [liked, setLiked] = React.useState(isAlreadyLiked);
   const Icon = liked ? UnlikeIcon : LikeIcon;
   const className = liked ? classes.liked : classes.like;
   const onClick = liked ? handleUnlike : handleLike;
+  const [likePost] = useMutation(LIKE_POST);
+  const [unlikePost] = useMutation(UNLIKE_POST);
+  const variables = {
+    postId,
+    userId: currentUserId,
+    profileId: authorId,
+  };
+
+  function handleUpdate(cache, result) {
+    const variables = { limit: 2, feedIds };
+    const data = cache.readQuery({
+      query: GET_FEED,
+      variables,
+    });
+    // console.log({ result, data });
+    const typename = result.data.insert_likes?.__typename;
+    const count = typename === 'likes_mutation_response' ? 1 : -1;
+    const posts = data.posts.map((post) => ({
+      ...post,
+      likes_aggregate: {
+        ...post.likes_aggregate,
+        aggregate: {
+          ...post.likes_aggregate.aggregate,
+          count: post.likes_aggregate.aggregate.count + count,
+        },
+      },
+    }));
+    cache.writeQuery({ query: GET_FEED, data: { posts } });
+  }
 
   function handleLike() {
-    console.log('like');
+    //console.log('like');
     setLiked(true);
+    likePost({ variables, update: handleUpdate });
   }
 
   function handleUnlike() {
-    console.log('unlike');
+    //console.log('unlike');
     setLiked(false);
+    unlikePost({ variables, update: handleUpdate });
   }
 
   return <Icon className={className} onClick={onClick} />;
 }
 
-function SaveButton() {
+function SaveButton({ postId, savedPosts }) {
   const classes = useFeedPostStyles();
   const [saved, setSaved] = React.useState(false);
   const Icon = saved ? RemoveIcon : SaveIcon;
